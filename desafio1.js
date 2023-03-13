f955e96f0874fef6a6c9164e46841734bf58fd84
RodrigoBarreiro <barreiro.rb@gmail.com>
2023-03-13 14:43:31 -0300

    arreglo de If

--- desafio1.js
+++ desafio1.js
@@ -26,7 +26,7 @@ class ProductManager
     addProduct(product)
     {
         const productFind = this.products.find ( p => product.code === p.code) // validacion de code
-        if (productFind != undefined){
+        if (productFind) {
             throw new Error ('Ya hay un producto con ese codigo');
         }
 
@@ -36,7 +36,7 @@ class ProductManager
     }
     getByProductId (idSearch) {
         const idFinde = this.products.find ( i => idSearch === i.id); 
-        if (idFinde === undefined) {
+        if (!idFinde) {
             throw new Error ('Not found');
         } 
         return idFinde;
@@ -46,8 +46,8 @@ class ProductManager
 const productManager = new ProductManager();
 
 productManager.addProduct ( new Product('Cerveza Negra', 'maltas oscuras provenientes del cereal malteado (humedecido, germinado y secado)' , 100 , 'sin imagen', 'A123' , 10 ));
-productManager.addProduct ( new Product('Cerveza Roja', 'De color cobre profundo con espuma densa y cremosa. aromas a caramelo con suaves notas a lúpulo. En boca se denota un leve dulzor y sabor a granos tostados.' , 190 , 'sin imagen', 'A122' , 30 ));
+productManager.addProduct ( new Product('Cerveza Roja', 'De color cobre profundo con espuma densa y cremosa. aromas a caramelo con suaves notas a lúpulo. En boca se denota un leve dulzor y sabor a granos tostados.' , 190 , 'sin imagen', 'A121' , 30 ));
 productManager.addProduct ( new Product('Cerveza IPA', 'cervezas de alta graduación alcohólica, al tener una mayor cantidad de lúpulo y por lo tanto un amargor  y aroma intensos y cierta complejidad en el paladar.' , 150 , 'sin imagen', 'A124' , 20 ));
 
 console.log(productManager.getProduct()); 
-console.log (productManager.getByProductId (1));
+console.log (productManager.getByProductId (2));
